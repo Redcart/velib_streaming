f26e40d56334c6f80549ac3c56edf9c93652628b
Simon Corde <simon.corde@hotmail.fr>
2020-10-03 15:38:00 +0200

    add kafka consumer & producer working for retrieving stations for Toulouse on one topic / one partition / one consumer group

---
+++ req.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.conf')
+
+API_KEY = config['VARIABLES']['API_KEY']
+# url = f'https://api.jcdecaux.com/vls/v3/contracts?apiKey={api_key}'
+
+
+# result = json.loads(requests.get(url).text)
+
+# print(type(result))
+# for contract in result:
+#     print(contract['name'])
+
+
+URL = f'https://api.jcdecaux.com/vls/v3/stations?contract=toulouse&apiKey={API_KEY}'
+
+
+result = json.loads(requests.get(URL).text)
+compteur = 0
+for station in result:
+    print(station['contractName'])
+    print(station['name'])
+    print(station['totalStands']['availabilities']['bikes'])
+    compteur += 1
+
+print(compteur)
---
+++ stations_producer.py
@@ -0,0 +1,30 @@
+from kafka import KafkaProducer
+import requests  
+import json
+import time
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.conf')
+
+API_KEY = config['VARIABLES']['API_KEY']
+URL = f'https://api.jcdecaux.com/vls/v3/stations?contract=toulouse&apiKey={API_KEY}'
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+
+while True:
+    response = json.loads(requests.get(URL).text)
+    for station in response:
+        producer.send("velib-stations", json.dumps(station).encode())
+    print("{} Produced {} station records".format(time.time(), len(response)))
+    time.sleep(1)
+
+# result = json.loads(requests.get(url).text.decode())
+# compteur = 0
+# for station in result:
+#     producer.send("velib-stations", json.dumps(station).encode())
+#     print(station['contractName'])
+#     print(station['name'])
+#     compteur += 1
+
+# print(compteur)
