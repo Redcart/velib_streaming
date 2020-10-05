19c7a9fc7bfe2f8754aed0c6ed328543cc6989f2
Simon Corde <simon.corde@hotmail.fr>
2020-10-05 17:09:20 +0200

    add connection to PostgreSQL Database for writing the output

--- stations_consumer.py
+++ stations_consumer.py
@@ -1,11 +1,49 @@
 import json
 from kafka import KafkaConsumer
+import psycopg2
 import pandas as pd 
+from connect_sql import config_sql
 
 stations = {}
 consumer = KafkaConsumer("velib-stations", bootstrap_servers='localhost:9092', group_id="velib-monitor-stations")
 FEATURES = ["station_number", "station_adress", "contract", "available_bike_stands"]
 df = pd.DataFrame(columns=FEATURES) 
+parameters_sql = config_sql()
+print(parameters_sql)
+conn = psycopg2.connect(**parameters_sql)
+print("SQL DataBase connection sucessful !")
+cur = conn.cursor()
+
+commands = [
+"""
+DROP TABLE IF EXISTS bikes_availabilty 
+""",
+"""
+DROP TABLE IF EXISTS bikes_availabilty_temp 
+""",
+""" CREATE TABLE bikes_availabilty (
+                station_number SERIAL PRIMARY KEY,
+                station_adress VARCHAR(255) NOT NULL,
+                contract VARCHAR(255) NOT NULL,
+                available_bike_stands INT NOT NULL
+                )
+ """,
+ """CREATE TABLE bikes_availabilty_temp (
+                station_number SERIAL PRIMARY KEY,
+                station_adress VARCHAR(255) NOT NULL,
+                contract VARCHAR(255) NOT NULL,
+                available_bike_stands INT NOT NULL
+                )
+"""
+]
+# create table one by one
+for command in commands:
+    cur.execute(command)
+# close communication with the PostgreSQL database server
+cur.close()
+# commit the changes
+conn.commit()
+print('SQL tables created ! ')
 
 for message in consumer:
     station_dico = dict()
@@ -36,3 +74,12 @@ for message in consumer:
         
         df = df.append(pd.DataFrame.from_dict(station_dico))
         df.to_csv('stations_availabilities.csv', index=None)
+
+        sql = """ INSERT INTO 
+                  bikes_availabilty (station_adress, contract, available_bike_stands) 
+                  VALUES(%s, %s, %s)
+              """
+        print((station["address"], station["contractName"], ))
+        cur = conn.cursor()
+        cur.execute(sql, (station["address"], station["contractName"], str(station['totalStands']['availabilities']['bikes'])))
+        conn.commit()
