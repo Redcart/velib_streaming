5347bd91e5ef5f10d3aa06206842d8d970ff5965
Simon Corde <simon.corde@hotmail.fr>
2020-10-06 16:34:16 +0200

    add scripts for persist results from temporary SQL table to permanent SQL table; require some cron job to write

---
+++ stations_static_info_persist.py
@@ -0,0 +1,39 @@
+import argparse
+import psycopg2
+from connect_sql import config_sql
+
+if __name__ == '__main__':
+
+    parameters_sql = config_sql()
+    conn = psycopg2.connect(**parameters_sql)
+    print("SQL DataBase connection sucessful !")
+    cur = conn.cursor()
+
+    sql = """
+              SELECT * FROM stations_info_temp
+          """
+  
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    sql = """ INSERT INTO 
+              stations_info (id, station_number, station_name, station_adress, contract, latitude, longitude, banking, status, connected, time) 
+              VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+          """
+
+    for row in results:
+        cur.execute(sql, row)
+
+    sql = """
+              DELETE FROM stations_info_temp 
+          """
+
+    cur.execute(sql)
+
+    # close communication with the PostgreSQL database server
+    cur.close()
+    # commit the changes
+    conn.commit()
+    print('SQL table append ! ')
+
+    
